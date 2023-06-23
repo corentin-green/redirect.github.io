24205fdb676ad5e8aa02401b3a8a8f3594b2634a
corentin <corentinway@gmail.com>
2023-06-23 15:09:48 +0200

    add CV link

--- redirections.js
+++ redirections.js
@@ -1,3 +1,4 @@
 const redirects = {
   hello: "https://hello.corentin.green",
+  cv: "https://docs.google.com/document/d/1pb1u36ZJzpCb18vbaWBd6Lqz-Q1R2OEE/edit?usp=sharing&ouid=100861321466353322782&rtpof=true&sd=true"
 };
