c244a87d7598cfa2ba68a7c7e111dbb2c39ec8db
corentin <corentinway@gmail.com>
2023-09-04 13:09:02 +0200

    Update redirections.js

--- redirections.js
+++ redirections.js
@@ -1,4 +1,5 @@
 const redirects = {
   hello: "https://hello.corentin.green",
-  cv: "https://docs.google.com/document/d/1pb1u36ZJzpCb18vbaWBd6Lqz-Q1R2OEE/edit?usp=sharing&ouid=100861321466353322782&rtpof=true&sd=true"
+  cv: "https://docs.google.com/document/d/1pb1u36ZJzpCb18vbaWBd6Lqz-Q1R2OEE/edit?usp=sharing&ouid=100861321466353322782&rtpof=true&sd=true",
+  b5: "https://filevert.fr/d/3d3IkN_5B_s_5D__5B_s_5D_AMDxSqP6IHQ=",
 };
